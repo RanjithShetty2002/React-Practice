e34ce486a23348402f0a0f429c97bec54798c828
RanjithShetty2002 <ranjithshetty2002@gmail.com>
2024-12-27 13:14:09 +0530

    Routing

---
+++ basics/learnstate/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function App() {
+  const [name, setName] = useState('Neha')
+
+  const handleclick = () => {
+    setName('Akash');
+  }
+  return (
+    <div>
+      <h1>Hello {name}</h1>
+      <button onClick ={handleclick}>Click</button>
+    </div>
+  )
+}
